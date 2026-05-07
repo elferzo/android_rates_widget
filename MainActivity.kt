a76bc53ce0b80bb4123748b62244a843a125c8f8
elferzo <94568971+elferzo@users.noreply.github.com>
2026-05-07 11:04:09 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -5,29 +5,25 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // Samsung One UI fix: request exact alarm permission (same pattern as DualClock v13)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val am = getSystemService(ALARM_SERVICE) as AlarmManager
             if (!am.canScheduleExactAlarms()) {
+                Toast.makeText(this, "Разрешите точные будильники для обновления виджета", Toast.LENGTH_LONG).show()
                 startActivity(Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM))
+            } else {
+                Toast.makeText(this, "Price Widget готов к работе", Toast.LENGTH_SHORT).show()
             }
+        } else {
+            Toast.makeText(this, "Price Widget готов к работе", Toast.LENGTH_SHORT).show()
         }
-        
-        // Trigger first data load immediately
-        val mgr = android.appwidget.AppWidgetManager.getInstance(this)
-        val ids = mgr.getAppWidgetIds(
-            android.content.ComponentName(this, PriceWidget::class.java)
-        )
-        for (id in ids) {
-            PriceWidget.updateWidget(this, mgr, id)
-        }
-        
+
         finish()
     }
 }
